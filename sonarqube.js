027042c4d170baca575aaec924ef9eb80381d63f
Mashal Aman <mashal.aman@tkxel.com>
2022-06-01 10:54:05 +0500

    New approach

--- sonarqube.js
+++ sonarqube.js
@@ -9,3 +9,19 @@ sonar-scanner \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://172.17.0.1:9000 \
   -Dsonar.login=1a0ef8686df35940ca12b43441232b59ec4b7894
+
+
+const scanner = require('sonarqube-scanner');
+
+scanner(
+  {
+    serverUrl : 'http://172.17.0.1:9000/',
+    token : "46220d4a56661af0fcfdd2827a651760e8accd2d",
+    options: {
+      'sonar.projectName': 'mashal',
+      'sonar.projectDescription': 'Assignment_6',
+    
+    }
+  },
+  () => process.exit()
+)
