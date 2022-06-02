941631652be4cb4632e637c79ca78425eec67428
Mashal Aman <mashal.aman@tkxel.com>
2022-06-02 10:56:33 +0500

    New approach-4

--- sonarqube.js
+++ sonarqube.js
@@ -4,11 +4,11 @@
 //   -Dsonar.host.url=http://172.17.0.1:9000 \
 //   -Dsonar.login=1a0ef8686df35940ca12b43441232b59ec4b7894
 
-sonar-scanner \
-  -Dsonar.projectKey=Jenkins \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://172.17.0.1:9000 \
-  -Dsonar.login=1a0ef8686df35940ca12b43441232b59ec4b7894
+// sonar-scanner \
+//   -Dsonar.projectKey=Jenkins \
+//   -Dsonar.sources=. \
+//   -Dsonar.host.url=http://172.17.0.1:9000 \
+//   -Dsonar.login=1a0ef8686df35940ca12b43441232b59ec4b7894
 
 
 const scanner = require('sonarqube-scanner');
@@ -16,9 +16,9 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
     serverUrl : 'http://172.17.0.1:9000/',
-    token : "46220d4a56661af0fcfdd2827a651760e8accd2d",
+    token : "cf81fb00755482a848ddabf42a430c2bfb2c343d",
     options: {
-      'sonar.projectName': 'mashal',
+      'sonar.projectName': 'Mashal',
       'sonar.projectDescription': 'Assignment_6',
     
     }
